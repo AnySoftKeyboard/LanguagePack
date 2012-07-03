bfddbf61afbba35a469a256164c016f55cb0040b
lado <herrlado@gmail.com>
2012-07-03 09:26:55 +0200

    initial

---
+++ src/org/herrlado/ask/languagepack/czech/KeyboardBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package org.herrlado.ask.languagepack.czech;
+
+import com.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
+
+public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
+
+}
