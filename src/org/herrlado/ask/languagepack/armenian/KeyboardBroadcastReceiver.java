bc0e396c765098cb6109db76499a8b1406482c60
lado <herrlado@gmail.com>
2014-02-17 23:36:19 +0100

    armenian new layout

---
+++ src/org/herrlado/ask/languagepack/armenian/KeyboardBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package org.herrlado.ask.languagepack.armenian;
+
+import com.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
+
+public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
+
+}
