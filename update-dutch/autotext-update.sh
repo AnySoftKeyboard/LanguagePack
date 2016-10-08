d3f8a2a84093fa9ffbb8f0640b29af6cfd4e8046
SvGeloven <sander.vangeloven@opentaal.org>
2016-10-08 19:14:20 +0200

    Omitted errors with spaces

--- update-dutch/autotext-update.sh
+++ update-dutch/autotext-update.sh
@@ -8,5 +8,6 @@ then
 fi
 
 cat templates/autotext-header.xml|sed -e 's/YYYY/'`date +%Y`'/g' > $TARGET
-cat data/fouten-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$5}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
+#cat data/fouten-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
+cat data/fouten-zonder-spaties-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
 cat templates/autotext-footer.xml >> $TARGET
