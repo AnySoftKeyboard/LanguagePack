5d1b1bd6c532245e512fe72967ab4f1b69e6a832
SvGeloven <sander.vangeloven@opentaal.org>
2016-10-08 11:49:38 +0200

    First contribution autotext for Dutch

---
+++ update-dutch/autotext-update.sh
@@ -0,0 +1,12 @@
+SOURCE=data/fouten-met-correcties.tsv
+TARGET=../src/main/res/xml/autotext.xml
+
+if [ ! -e $SOURCE ]
+then
+    echo 'ERROR: Missing file '$SOURCE
+    exit 1
+fi
+
+cat templates/autotext-header.xml|sed -e 's/YYYY/'`date +%Y`'/g' > $TARGET
+cat data/fouten-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$5}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
+cat templates/autotext-footer.xml >> $TARGET
