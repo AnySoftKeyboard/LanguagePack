1449557660f095820346e8227c6efb8ea2f8ec24
SvGeloven <sander.vangeloven@opentaal.org>
2016-10-09 23:21:33 +0200

    Added d-t errors

--- update-dutch/autotext-update.sh
+++ update-dutch/autotext-update.sh
@@ -2,7 +2,7 @@
 
 # See also fouten-zonder-spaties-met-correcties.sh on zapf.ntg.nl
 
-SOURCE=data/fouten-met-correcties.tsv
+SOURCE=data/fouten-zonder-spaties-met-correcties.tsv
 TARGET=../src/main/res/xml/autotext.xml
 
 if [ ! -e $SOURCE ]
@@ -12,6 +12,5 @@ then
 fi
 
 cat resources/autotext-header.xml|sed -e 's/YYYY/'`date +%Y`'/g' > $TARGET
-#cat data/fouten-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
-cat data/fouten-zonder-spaties-met-correcties.tsv|sed -e 's/;.*//g'|sed -e 's/&/&amp;/g'|sed -e 's/"/&quot;/g'|sed -e 's/</&lt;/g'|sed -e 's/>/&gt;/g'|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/$/<\/word>/g' >> $TARGET
+cat $SOURCE|sed -e 's/;.*//g'|sed -e 's/&/&amp;/g'|sed -e 's/"/&quot;/g'|sed -e 's/</&lt;/g'|sed -e 's/>/&gt;/g'|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/$/<\/word>/g' >> $TARGET
 cat resources/autotext-footer.xml >> $TARGET
