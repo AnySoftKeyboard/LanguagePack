0b5db1cecc89ab04525c3129863b844c2399cc23
SvGeloven <sander.vangeloven@opentaal.org>
2016-10-09 14:21:48 +0200

    Improved icons

--- update-dutch/autotext-update.sh
+++ update-dutch/autotext-update.sh
@@ -13,5 +13,5 @@ fi
 
 cat resources/autotext-header.xml|sed -e 's/YYYY/'`date +%Y`'/g' > $TARGET
 #cat data/fouten-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
-cat data/fouten-zonder-spaties-met-correcties.tsv|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/;.*//g'|sed -e 's/$/<\/word>/g' >> $TARGET
+cat data/fouten-zonder-spaties-met-correcties.tsv|sed -e 's/;.*//g'|sed -e 's/&/&amp;/g'|sed -e 's/"/&quot;/g'|sed -e 's/</&lt;/g'|sed -e 's/>/&gt;/g'|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/$/<\/word>/g' >> $TARGET
 cat resources/autotext-footer.xml >> $TARGET
--- update-dutch/icon-update.sh
+++ update-dutch/icon-update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# See also https://nl.wikipedia.org/wiki/Bestand:Flag_of_the_Netherlands.svg
+# See also
+# https://nl.wikipedia.org/wiki/Bestand:Flag_of_the_Netherlands.svg
+# https://commons.wikimedia.org/wiki/File:Flag_of_Belgium.svg
+# https://commons.wikimedia.org/wiki/File:Flag_of_Suriname.svg
 
 SOURCE=resources/store_hi_res_icon.png
 TARGET=../StoreStuff/store_hi_res_icon.png
@@ -12,3 +15,6 @@ then
 fi
 
 cp -f $SOURCE $TARGET
+convert $SOURCE -resize 48x48 ../src/main/res/drawable/app_icon.png
+convert $SOURCE -resize 72x72 ../src/main/res/drawable-hdpi/app_icon.png
+convert $SOURCE -resize 96x96 ../src/main/res/drawable-xhdpi/app_icon.png
