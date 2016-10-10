4ea277a7370eb2bc75ddf13b314d1e48abf0dead
SvGeloven <sander.vangeloven@opentaal.org>
2016-10-10 10:37:34 +0200

    Added dt errors and fixed d-t bug

--- update-dutch/autotext-update.sh
+++ update-dutch/autotext-update.sh
@@ -11,6 +11,9 @@ then
     exit 1
 fi
 
+echo 'INFO: Number of lines in source is '`wc -l $SOURCE`
+echo 'INFO: Number of lines in target before update is '`wc -l $TARGET`
 cat resources/autotext-header.xml|sed -e 's/YYYY/'`date +%Y`'/g' > $TARGET
 cat $SOURCE|sed -e 's/;.*//g'|sed -e 's/&/&amp;/g'|sed -e 's/"/&quot;/g'|sed -e 's/</&lt;/g'|sed -e 's/>/&gt;/g'|awk -F '\t' '{print "    <word src=\""$3"\">"$4}'|sed -e 's/$/<\/word>/g' >> $TARGET
 cat resources/autotext-footer.xml >> $TARGET
+echo 'INFO: Number of lines in target after update is '`wc -l $TARGET`
