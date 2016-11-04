04548e30c077adf9c913ba96bc8b32fa3c277a46
SvGeloven <sander.vangeloven@opentaal.org>
2016-11-04 16:31:54 +0100

    Initial complete contribution

---
+++ update-dutch/words-update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# See also TODO on zapf.ntg.nl
+
+python3 ./scale-word-list.py
+
+SOURCE=data/words-scaled.tsv
+HEADER=resources/words-header.xml
+FOOTER=resources/words-footer.xml
+TARGET=../dictionary/words.xml
+
+if [ ! -e $SOURCE ]
+then
+    echo 'ERROR: Missing file '$SOURCE
+    exit 1
+fi
+
+echo 'INFO: Number of lines in source is '`wc -l $SOURCE`
+echo 'INFO: Number of lines in target before update is '`wc -l $TARGET`
+sed 's/YYYY/'`date +%Y`'/g' $HEADER > $TARGET
+sed 's/&/&amp;/g' $SOURCE|sed 's/"/&quot;/g'|sed 's/</&lt;/g'|sed 's/>/&gt;/g'|awk -F '\t' '{print "    <w f=\""$1"\">"$2}'|sed 's/$/<\/w>/g' >> $TARGET
+cat $FOOTER >> $TARGET
+echo 'INFO: Number of lines in target after update is '`wc -l $TARGET`
+
+java -jar ../../AnySoftKeyboardTools/makedict/makedict.jar
