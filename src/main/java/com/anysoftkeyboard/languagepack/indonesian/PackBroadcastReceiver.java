4be67723cfbe0ab34d8d9bd7b3e17b987ee9e8a3
azzamsa <oktinastuzi@gmail.com>
2018-02-02 08:38:55 +0700

    update for insonesian languagepack

--- src/main/java/com/anysoftkeyboard/languagepack/indonesian/PackBroadcastReceiver.java
+++ src/main/java/com/anysoftkeyboard/languagepack/indonesian/PackBroadcastReceiver.java
@@ -1,4 +1,4 @@
-package com.anysoftkeyboard.languagepack.languagepack;
+package com.anysoftkeyboard.languagepack.indonesian;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
