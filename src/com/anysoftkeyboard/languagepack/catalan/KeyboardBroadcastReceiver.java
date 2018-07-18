ffdc60b722ecb84aacd37b159cb1c228e626381d
friesenkiwi <git@friesenkiwi.de>
2018-07-18 05:44:32 +0200

    resurrect old SVN sources

---
+++ src/com/anysoftkeyboard/languagepack/catalan/KeyboardBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.anysoftkeyboard.languagepack.catalan;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeyboardBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}
