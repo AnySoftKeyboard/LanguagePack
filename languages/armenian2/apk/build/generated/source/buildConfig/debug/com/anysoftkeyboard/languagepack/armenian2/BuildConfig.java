11a25fb1c4145f79892a91633d43a27226415606
mzeinali <mzeinali@protonmail.com>
2019-09-30 14:47:19 +0330

    reinit git

---
+++ languages/armenian2/apk/build/generated/source/buildConfig/debug/com/anysoftkeyboard/languagepack/armenian2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.anysoftkeyboard.languagepack.armenian2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.anysoftkeyboard.languagepack.armenian2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "4.0.1";
+}
