446d62473cdd24ace93f47e469b8aec74a21c55e
Miroslav Novta <miroslav@opticaltone.com>
2020-01-28 12:07:06 +0100

    Code review suggestions are considered and fixed.

--- languages/rusin/pack/src/main/java/com/anysoftkeyboard/languagepack/rusin/PackBroadcastReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.anysoftkeyboard.languagepack.rusin;
-
-import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
-
-public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
-
-}
